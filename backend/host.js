a6199e6a11f1c14e9438d6257305b260822a4147
tkruse <tkruse@deltamodtech.com>
2024-01-05 17:31:01 -0600

    Update Trello Ids
    Add trello ids and api key.

--- backend/host.js
+++ backend/host.js
@@ -2,7 +2,7 @@ const express = require('express');
 const trelloManager = require('./trello.js');
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 // Disable x-powered-by header (hides that express is being used)
 app.disable('x-powered-by');
