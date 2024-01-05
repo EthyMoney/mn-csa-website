e982b092cf7f93134372b73d90ada2d7d64388df
EthyMoney <ultimate360solutions@gmail.com>
2024-01-05 13:04:43 -0600

    cleaned up a bit

--- backend/trello.js
+++ backend/trello.js
@@ -8,17 +8,18 @@ const trelloLabels = config.trelloBoardLabels;
 
 // create a new card on the incoming list for the specified board based on event
 async function createCard(title, teamNumber, frcEvent, problemCategory, priority, description, attachments) {
-  // find the id of the board in trelloBoards where the name matches the frcEvent
+  // find the id of the board we want to create the card on according to the selected event
   const boardId = trelloBoards.find(board => board.frontendEventSelection.toLowerCase() === frcEvent.toLowerCase()).boardID;
-  // find the id of the incoming list on the board so we can create the card there
+
+  // find the id of the "incoming" list on the board so we can create the card there
   const listId = await getIncomingListIdOfBoard(boardId);
-  // create a formatted and detailed description for the card
   const formattedDescription = `**This is a submitted request that was added automatically**\n\nTeam Number: ${teamNumber}\n\nCategory: ${problemCategory}\n\nPriority: ${priority}\n\nDescription: ${description}`;
-  // need to add labels for the problem category and priority level, so get the ids of the labels
+
+  // lookup the IDs of the labels we want to add to the card based on the selected category and priority
   const problemCategoryLabelId = await getLabelIdByName(boardId, problemCategory);
   const priorityLabelId = await getLabelIdByName(boardId, priority);
 
-  // create the card
+  // send the request to create the new card, it will be created at the top of the "incoming" list
   const res = await fetch(`https://api.trello.com/1/cards?key=${config.trelloAppKey}&token=${config.trelloUserToken}`, {
     method: 'POST',
     headers: {
@@ -51,35 +52,21 @@ async function createCard(title, teamNumber, frcEvent, problemCategory, priority
 }
 
 
-// returns the id of the incoming list on the board
-async function getIncomingListIdOfBoard(boardId) {
-  const res = await fetch(`https://api.trello.com/1/boards/${boardId}/lists?key=${config.trelloAppKey}&token=${config.trelloUserToken}`);
-  if (res.ok) {
-    const resJson = await res.json();
-    return resJson.find(list => list.name.toLowerCase() === "incoming").id;
-  }
-  else {
-    return;
-  }
-}
 
-
-// trelloLabels is an array of all of the labels we expect to see in each board, if we don't see one in the board matching the one in the array, we will create it
+// verify that our configured labels exist on all our configured boards, create them if they don't 
+//! (label creation not working because trello hates me I guess, literally no idea why. This is exactly how it's done in the docs. Looking up labels works, creating them doesn't and complains about the board ID. WHY?)
 async function verifyLabels() {
-  // iterate through each board
   for (const board of trelloBoards) {
-    // get the id of the board
     const boardId = board.boardID;
-    // get the labels on the board
+    // grab the labels currently present on the board, if any
     const res = await fetch(`https://api.trello.com/1/boards/${boardId}/labels?key=${config.trelloAppKey}&token=${config.trelloUserToken}`);
     if (res.ok) {
       const resJson = await res.json();
       console.log(`Found ${resJson.length} labels on board ${boardId}`);
-      // iterate through each label in trelloLabels
+      // iterate through each label in our local config
       for (const expectedLabel of trelloLabels) {
-        // if we don't see the label in the board, create it
+        // if we don't see our label on the board, create it
         if (!resJson.find(boardLabel => boardLabel.name.toLowerCase() === expectedLabel.name.toLowerCase())) {
-          //console.log(`https://api.trello.com/1/labels?key=${config.trelloAppKey}&token=${config.trelloUserToken}&name=${encodeURIComponent(expectedLabel.name)}&color=${expectedLabel.color}&idBoard=${boardId}`)
           const res = await fetch(`https://api.trello.com/1/labels?key=${config.trelloAppKey}&token=${config.trelloUserToken}&name=${encodeURIComponent(expectedLabel.name)}&color=${expectedLabel.color}&idBoard=${boardId}`, {
             method: 'POST'
           });
@@ -101,7 +88,22 @@ async function verifyLabels() {
 }
 
 
-// look up label id by name
+
+// lookup and return the list ID of the "incoming" list on the specified board
+async function getIncomingListIdOfBoard(boardId) {
+  const res = await fetch(`https://api.trello.com/1/boards/${boardId}/lists?key=${config.trelloAppKey}&token=${config.trelloUserToken}`);
+  if (res.ok) {
+    const resJson = await res.json();
+    return resJson.find(list => list.name.toLowerCase() === "incoming").id;
+  }
+  else {
+    return;
+  }
+}
+
+
+
+// lookup and return the ID of the label with the specified name on the specified board
 async function getLabelIdByName(boardId, labelName) {
   const res = await fetch(`https://api.trello.com/1/boards/${boardId}/labels?key=${config.trelloAppKey}&token=${config.trelloUserToken}`);
   if (res.ok) {
