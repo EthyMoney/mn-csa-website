77f59d8c5ee7bd1a8f90bcfb3643c510016f20a8
EthyMoney <ultimate360solutions@gmail.com>
2024-03-29 14:20:43 -0500

    started implementing logging module in backend

--- backend/host.js
+++ backend/host.js
@@ -1,11 +1,14 @@
 const preCheck = require('./pre-checks.js');
 preCheck();
+const config = require("../config/config.json");
 const express = require('express');
 const trelloManager = require('./trello.js');
+const chalk = require('chalk');
 const path = require('path');
+const { writeToLogFile } = require('./logger.js');
 
 const app = express();
-const port = 3000;
+const port = config.appPort;
 
 // Disable x-powered-by header (hides that express is being used)
 app.disable('x-powered-by');
@@ -19,6 +22,16 @@ app.use(express.json());
 
 // Log all requests
 app.use((req, res, next) => {
+  if (req.path === '/') {
+    console.log(`${req.method} ${req.path} - Page load`);
+    next();
+    return;
+  }
+  // if a static file is requested, don't log it (I don't want to see all the requests for images, css, etc.)
+  if (req.path.includes('.')) {
+    next();
+    return;
+  }
   console.log(`${req.method} ${req.path}`);
   next();
 });
@@ -30,6 +43,7 @@ app.post('/submit', (req, res) => {
   // Create a new Trello card using the data received from frontend form
   console.log('Received card data:');
   console.log({ ...req.body, attachments: req.body.attachments.length });
+  writeToLogFile(`Received card data: ${JSON.stringify(req.body)}`, 'info', 'host.js', '/submit');
   trelloManager.createCard(req.body.title, req.body.teamNumber, req.body.contactEmail, req.body.contactName, req.body.frcEvent, req.body.problemCategory, req.body.priority, req.body.description, req.body.attachments);
   res.status(200).send('Request received successfully!');
 });
